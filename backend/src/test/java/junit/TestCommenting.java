94014ed8cec650f1e37fc209842d5cac04f2ca30
Holly <hollyvc05@gmail.com>
2025-11-19 23:42:14 +0100

    Fixed issue with guests commenting and getting stuck with popups

---
+++ backend/src/test/java/junit/TestCommenting.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class testComment {
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+}
---
+++ backend/src/test/java/junit/TestLoggingIn.java
@@ -0,0 +1,17 @@
+package org.junit.
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class testLogIn {
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+}
