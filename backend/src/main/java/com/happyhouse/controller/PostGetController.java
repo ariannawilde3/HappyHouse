2b66d7f096e400fc84886467fb8b818f222e96fb
Leili Nelson <squeezy6@gmail.com>
2025-11-20 11:40:36 +0100

    forgot to add this in last commit

--- backend/src/main/java/com/happyhouse/controller/PostGetController.java
+++ backend/src/main/java/com/happyhouse/controller/PostGetController.java
@@ -3,20 +3,14 @@ package com.happyhouse.controller;
 //TODO: post list just returns previews
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.happyhouse.model.Post;
 import com.happyhouse.repository.PostListTesting;
 import com.happyhouse.repository.PostRepository;
 import org.springframework.data.domain.*;
-import com.happyhouse.dto.AddPostRequest;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
-
-
-import java.net.URI;
 
 @RestController
 @RequestMapping("/api/viewpost")
@@ -30,8 +24,6 @@ public class PostGetController {
 
 	@GetMapping("/all/{page}")
 	public List<Post> getPostList(@PathVariable int page) {
-		//return postlist.getAllPosts();
-		//return postrepo.findAll();
 		Pageable pageable = PageRequest.of(page, 3, Sort.by("_id").descending());
 		return postrepo.findAll(pageable).getContent();
 	}
@@ -47,7 +39,7 @@ public class PostGetController {
 		Post newPost = new Post(
 		"test content",
 		"test title",
-		new ArrayList(Arrays.asList("test tag", "two tag")));
+		new ArrayList<String>(Arrays.asList("test tag", "two tag")));
 		
 		postrepo.save(newPost);
 		return newPost;
